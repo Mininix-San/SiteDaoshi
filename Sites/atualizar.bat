a9d3b4c26328b34f0436ffffd18665270dc58c5f
Mininix <gabrielqueirozreis@outlook.com>
2025-10-30 19:29:35 -0300

    nada

--- Sites/atualizar.bat
+++ Sites/atualizar.bat
@@ -1,7 +1,17 @@
 @echo off
 cd /d D:\Cursor\Sites
+
+echo ============================
+echo    GIT COMMIT AUTOMATICO
+echo ============================
+echo.
+
 git add .
+echo.
 set /p msg=Mensagem do commit: 
+
 git commit -m "%msg%"
 git push
+
+echo.
 pause
